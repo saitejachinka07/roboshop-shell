6883617796ca144beadde4a62de29ae4a160c7fd
saitej <chinka.saiteja@gmail.com>
2023-04-20 22:06:34 +0530

    sed commit

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,7 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y 
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 systemctl enable mongod
 systemctl start mongod
  
- # edit the file and replace 127.0.0.1 to 0.0.0.0
+
