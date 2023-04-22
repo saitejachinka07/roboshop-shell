d0bb3a44556d648e5fcebd3f5cca66ae0e8e7723
saitej <chinka.saiteja@gmail.com>
2023-04-22 22:43:08 +0530

    redis conf commit

--- Redis.sh
+++ Redis.sh
@@ -4,6 +4,6 @@ yum install redis -y
 echo -e "\e[35m>>>>>>edit REDIS.CONF<<<<<<<<\e[0m"
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
 echo -e "\e[35m>>>>>>edit REDIS.2.CONF<<<<<<<<\e[0m"
-sed -i -e 's|127.0.0.0|0.0.0.0|' /etc/redis/redis.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis/redis.conf
 systemctl enable redis 
 systemctl restart redis 
