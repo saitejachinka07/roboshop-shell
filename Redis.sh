db51ce2458be99d9171dbca48db5beecc56d74f4
saitej <chinka.saiteja@gmail.com>
2023-04-22 22:37:06 +0530

    Redis.2 conf commit

--- Redis.sh
+++ Redis.sh
@@ -1,6 +1,9 @@
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 dnf module enable redis:remi-6.2 -y
 yum install redis -y
+echo -e "\e[35m>>>>>>edit REDIS.CONF<<<<<<<<\e[0m"
 sed -i -e 's|127.0.0.0|0.0.0.0|' /etc/redis.conf
+echo -e "\e[35m>>>>>>edit REDIS.2.CONF<<<<<<<<\e[0m"
+sed -i -e 's|127.0.0.0|0.0.0.0|' /etc/redis/redis.conf
 systemctl enable redis 
 systemctl restart redis 
