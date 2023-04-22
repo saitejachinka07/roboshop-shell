043464fb2061b41ae8f3618c4adf99010bcb3a24
saitej <chinka.saiteja@gmail.com>
2023-04-22 12:12:59 +0530

    step by step color commit

--- catalogue.sh
+++ catalogue.sh
@@ -1,16 +1,29 @@
+echo -e "\e[35m>>>>>>>>>>>>configuring nodejs repo<<<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo "\e[35m>>>>>>>>>>>>installing nodejs<<<<<<<<<<<\e[0m"
 yum install nodejs -y
+echo -e "\e[35m>>>>>>>>>>>>adding useradd<<<<<<<<<<<\e[0m"
 useradd roboshop
+echo -e "\e[35m>>>>>>>>>>>>removing content in /app<<<<<<<<<<<\e[0m"
 rm -rf /app
+echo -e "\e[35m>>>>>>>>>>>>creating app directory<<<<<<<<<<<\e[0m"
 mkdir /app
+echo -e "\e[35m>>>>>>>>>>>>downloading app content<<<<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
 cd /app 
+echo -e "\e[35m>>>>>>>>>>>>unzip app content<<<<<<<<<<<\e[0m"
 unzip /tmp/catalogue.zip
+echo -e "\e[35m>>>>>>>>>>>>install nodejs dependencies<<<<<<<<<<<\e[0m"
 npm install
 cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
+echo -e "\e[35m>>>>>>>>>>>>reloading daemon<<<<<<<<<<<\e[0m"
 systemctl daemon-reload
+echo -e "\e[35m>>>>>>>>>>>>enabling catalogue service<<<<<<<<<<<\e[0m"
 systemctl enable cataloggue
 systemctl start cataloggue
+echo -e "\e[35m>>>>>>>>>>>>downloading mongo repo<<<<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.d
+echo -e "\e[35m>>>>>>>>>>>>installing mongodb<<<<<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
+echo -e "\e[35m>>>>>>>>>>>>creating mongodb db schema<<<<<<<<<<<\e[0m"
 mongo --host mongodb.tej07.online </app/schema/catalogue.js
