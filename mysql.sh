454977ec87251a610258628d87b0e06d2e8cf82b
saitej <chinka.saiteja@gmail.com>
2023-04-23 16:08:48 +0530

    mysqld commit

--- mysql.sh
+++ mysql.sh
@@ -5,9 +5,9 @@ cp /home/centos/roboshop-shell/mysql.repo  /etc/yum.repos.d/mysql.repo
 echo -e "\e[36m>>>>>>>>> installing mysql <<<<<<<<<<\e[0m"
 yum install mysql-community-server -y
 echo -e "\e[36m>>>>>>>>> enabling mysql service <<<<<<<<<<\e[0m"
-systemctl enable mysql
+systemctl enable mysqld
 echo -e "\e[36m>>>>>>>>> start mysql service <<<<<<<<<<\e[0m"
-systemctl start mysql
+systemctl start mysqld
 echo -e "\e[36m>>>>>>>>> Reset Mysql password <<<<<<<<<<\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
 #mysql -uroot -pRoboShop@1
