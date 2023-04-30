c45ff82866bc5df3063cb8c64cac8b552dc08596
saitej <chinka.saiteja@gmail.com>
2023-04-30 17:20:33 +0530

    realpath

--- User.sh
+++ User.sh
@@ -1,5 +1,17 @@
+script=$(realpath "$0")
+
+realpath $0
+
+exit
+
+
+
+
+
 script_path=$(dirname $0)
 source ${script_path}/common.sh
+
+
 
 
 echo -e "\e[36m>>>>>>>>>>>>creating nodejs repo file<<<<<<<<<<<\e[0m"
