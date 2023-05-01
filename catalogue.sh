22127130b142496229ce97f9e52af22dc65aa7e7
saitej <chinka.saiteja@gmail.com>
2023-05-01 18:17:51 +0530

    rename component Cart to cart

--- catalogue.sh
+++ catalogue.sh
@@ -1,6 +1,7 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+
 component=catalogue
 func_nodejs
 echo -e "\e[35m>>>>>>>>>>>>downloading mongo repo<<<<<<<<<<<\e[0m"
