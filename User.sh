7462c41767310f231c0f45f46a8d707db8cd919f
saitej <chinka.saiteja@gmail.com>
2023-05-01 17:15:59 +0530

    func_nodejs

--- User.sh
+++ User.sh
@@ -1,37 +1,11 @@
  script=$(realpath "$0")
  script_path=$(dirname "$script")
-
-
 source ${script_path}/common.sh
 
+component=user
 
+func_nodejs
 
-
-echo -e "\e[36m>>>>>>>>>>>>creating nodejs repo file<<<<<<<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-
-echo -e "\e[35m>>>>>>>>>>>> installing nodejs <<<<<<<<<<<\e[0m"
-yum install nodejs -y
-echo -e "\e[35m>>>>>>>>>>>> creating roboshop user <<<<<<<<<<<\e[0m"
-useradd ${app_user}
-echo -e "\e[35m>>>>>>>>>>>> removing /app folder <<<<<<<<<<<\e[0m"
-rm -rf /app
-echo -e "\e[35m>>>>>>>>>>>> creating application folder <<<<<<<<<<<\e[0m"
-mkdir /app
-echo -e "\e[35m>>>>>>>>>>>> downloading app content <<<<<<<<<<<\e[0m"
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip 
-cd /app 
-echo -e "\e[35m>>>>>>>>>>>> unziping the app content <<<<<<<<<<<\e[0m"
-unzip /tmp/user.zip
-echo -e "\e[35m>>>>>>>>>>>> installing nodejs dependencies <<<<<<<<<<<\e[0m"
-npm install
-echo -e "\e[35m>>>>>>>>>>>> copying service file <<<<<<<<<<<\e[0m"
-cp ${script_path}/user.service  /etc/systemd/system/user.service
-echo -e "\e[35m>>>>>>>>>>>> realoading daemon <<<<<<<<<<<\e[0m"
-systemctl daemon-reload
-echo -e "\e[35m>>>>>>>>>>>> enable user  <<<<<<<<<<<\e[0m"
-systemctl enable user
-systemctl start user
 echo -e "\e[35m>>>>>>>>>>>>copy mongo.repo file <<<<<<<<<<<\e[0m"
 cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 echo -e "\e[35m>>>>>>>>>>>> installing mongodb  <<<<<<<<<<<\e[0m"
