3e01af225d6de66d48fc2b62f29c15ab44aca24b
saitej <chinka.saiteja@gmail.com>
2023-05-01 17:53:05 +0530

    catalogue func_nodjs

--- catalogue.sh
+++ catalogue.sh
@@ -1,30 +1,8 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-echo -e "\e[35m>>>>>>>>>>>>configuring nodejs repo<<<<<<<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-echo -e "\e[35m>>>>>>>>>>>>installing nodejs<<<<<<<<<<<\e[0m"
-yum install nodejs -y
-echo -e "\e[35m>>>>>>>>>>>>adding useradd<<<<<<<<<<<\e[0m"
-useradd ${app_user}
-echo -e "\e[35m>>>>>>>>>>>>removing content in /app<<<<<<<<<<<\e[0m"
-rm -rf /app
-echo -e "\e[35m>>>>>>>>>>>>creating app directory<<<<<<<<<<<\e[0m"
-mkdir /app
-echo -e "\e[35m>>>>>>>>>>>>downloading app content<<<<<<<<<<<\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
-cd /app 
-echo -e "\e[35m>>>>>>>>>>>>unzip app content<<<<<<<<<<<\e[0m"
-unzip /tmp/catalogue.zip
-echo -e "\e[35m>>>>>>>>>>>>install nodejs dependencies<<<<<<<<<<<\e[0m"
-npm install
-echo -e "\e[36m>>>>>>>>>>>> copying catalogue.service file <<<<<<<<<<<<<<<<\e[0m"
-cp ${script_path}/catalogue.service1  /etc/systemd/system/catalogue.service
-echo -e "\e[35m>>>>>>>>>>>>reloading daemon<<<<<<<<<<<\e[0m"
-systemctl daemon-reload
-echo -e "\e[35m>>>>>>>>>>>>enabling catalogue service<<<<<<<<<<<\e[0m"
-systemctl enable catalogue
-systemctl start catalogue
+component=catalogue
+func_nodejs
 echo -e "\e[35m>>>>>>>>>>>>downloading mongo repo<<<<<<<<<<<\e[0m"
 cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 echo -e "\e[35m>>>>>>>>>>>>installing mongodb-shell<<<<<<<<<<<\e[0m"
