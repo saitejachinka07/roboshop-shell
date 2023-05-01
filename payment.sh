4245cd461290c36cb4a91d127f0a84ebb3567bf2
saitej <chinka.saiteja@gmail.com>
2023-05-01 16:38:15 +0530

    sed "s" closer deliminator commit

--- payment.sh
+++ payment.sh
@@ -31,7 +31,7 @@ pip3.6 install -r requirements.txt
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>   copying payment.service file   <<<<<<<<<<<<<<<<<<\e[0m"
 
-sed -i -e  "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}" ${script_path}/payment.service
+sed -i -e  "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|" ${script_path}/payment.service
 cp ${script_path}/payment.service /etc/systemd/system/payment.service
 
 #sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|"  $script_path}/payment.service
