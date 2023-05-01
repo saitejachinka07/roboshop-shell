27601139d4d5fcfd59d0f441bdccaf69abfcedf6
saitej <chinka.saiteja@gmail.com>
2023-05-01 17:14:04 +0530

    func_nodejs User.sh

--- common.sh
+++ common.sh
@@ -1,3 +1,33 @@
 app_user=roboshop
 script=$(realpath "$0")
 script_path=$(dirname "$script")
+
+
+
+func_nodejs() {
+    echo -e "\e[36m>>>>>>>>>>>>creating nodejs repo file<<<<<<<<<<<\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[35m>>>>>>>>>>>> installing nodejs <<<<<<<<<<<\e[0m"
+yum install nodejs -y
+echo -e "\e[35m>>>>>>>>>>>> creating roboshop user <<<<<<<<<<<\e[0m"
+useradd ${app_user}
+echo -e "\e[35m>>>>>>>>>>>> removing /app folder <<<<<<<<<<<\e[0m"
+rm -rf /app
+echo -e "\e[35m>>>>>>>>>>>> creating application folder <<<<<<<<<<<\e[0m"
+mkdir /app
+echo -e "\e[35m>>>>>>>>>>>> downloading app content <<<<<<<<<<<\e[0m"
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip 
+cd /app 
+echo -e "\e[35m>>>>>>>>>>>> unziping the app content <<<<<<<<<<<\e[0m"
+unzip /tmp/${component}.zip
+echo -e "\e[35m>>>>>>>>>>>> installing nodejs dependencies <<<<<<<<<<<\e[0m"
+npm install
+echo -e "\e[35m>>>>>>>>>>>> copying service file <<<<<<<<<<<\e[0m"
+cp ${script_path}/${component}.service  /etc/systemd/system/${component}.service
+echo -e "\e[35m>>>>>>>>>>>> realoading daemon <<<<<<<<<<<\e[0m"
+systemctl daemon-reload
+echo -e "\e[35m>>>>>>>>>>>> enable user  <<<<<<<<<<<\e[0m"
+systemctl enable ${component}
+systemctl start ${component}
+}
