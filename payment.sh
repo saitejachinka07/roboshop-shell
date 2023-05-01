992a9697d3dac065c62f0cfcb2dbd89044eba01e
saitej <chinka.saiteja@gmail.com>
2023-05-01 16:02:27 +0530

    not declaired payment variable

--- payment.sh
+++ payment.sh
@@ -1,6 +1,7 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+rabbitmq_appuser_password=$1
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>   Installing python   <<<<<<<<<<<<<<<<<<\e[0m"
 yum install python36 gcc python3-devel -y
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>   creatig roboshop user   <<<<<<<<<<<<<<<<<<\e[0m"
@@ -30,11 +31,11 @@ pip3.6 install -r requirements.txt
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>   copying payment.service file   <<<<<<<<<<<<<<<<<<\e[0m"
 
-#sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|"  $script_path}/payment.service
+sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|"  $script_path}/payment.service
 
 cp ${script_path}/payment.service /etc/systemd/system/payment.service
 
-sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|"  $script_path}/payment.service
+#sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|"  $script_path}/payment.service
 
 
 echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>   Reloading daemon   <<<<<<<<<<<<<<<<<<\e[0m"
