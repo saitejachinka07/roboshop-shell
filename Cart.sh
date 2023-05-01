d179d938acacd7507cc3f43c02a0316457c2b90e
saitej <chinka.saiteja@gmail.com>
2023-05-01 17:43:37 +0530

    func_nodejs and realpath

--- Cart.sh
+++ Cart.sh
@@ -1,8 +1,6 @@
-script_path=$(dirname $0)
+script=$(realpath "$0")
+script_path=$(dirname "$script")
 source ${script_path}/common.sh
-echo -e "\e[36m>>>>>>>>>>>>creating nodejs repo file<<<<<<<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-echo -e "\e[35m>>>>>>>>>>>> installing nodejs <<<<<<<<<<<\e[0m"
 
 component=cart
 
