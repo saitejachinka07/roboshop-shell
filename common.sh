2413693e592e4b65e925aac3d8769d86e71bd217
saitej <chinka.saiteja@gmail.com>
2023-05-01 18:56:32 +0530

    print_head "" commit

--- common.sh
+++ common.sh
@@ -5,36 +5,36 @@ script_path=$(dirname "$script")
 
 print_head(){
 
-echo -e "\e[36m>>>>>>>>>>>>>>>> $* <<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>> $1 <<<<<<<<<<<\e[0m"
 
 
 }
 
 
 func_nodejs() {
-  print_head   creating nodejs repo file
+  print_head   "creating nodejs repo file"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-  print_head installing nodejs 
+  print_head "installing nodejs" 
 yum install nodejs -y
-  print_head creating roboshop user 
+  print_head "creating roboshop user" 
 useradd ${app_user}
- print_head removing /app folder 
+ print_head "removing /app folder" 
 rm -rf /app
- print_head creating application folder 
+ print_head "creating application folder" 
 mkdir /app
- print_head downloading app content 
+ print_head "downloading app content"
 curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip 
 cd /app 
- print_head unziping the app content 
+ print_head "unziping the app content" 
 unzip /tmp/${component}.zip
- print_head installing nodejs dependencies 
+ print_head "installing nodejs dependencies" 
 npm install
- print_head copying service file 
+ print_head "copying service file "
 cp ${script_path}/${component}.service  /etc/systemd/system/${component}.service
- print_head realoading daemon 
+ print_head "realoading daemon" 
 systemctl daemon-reload
- print_head enable user  
+ print_head "enable user"  
 systemctl enable ${component}
 systemctl start ${component}
 }
