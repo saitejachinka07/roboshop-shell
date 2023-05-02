25ae44e26738119d846b94533eea738c831bd344
saitej <chinka.saiteja@gmail.com>
2023-05-02 16:05:52 +0530

    schema_setup_user

--- User.sh
+++ User.sh
@@ -6,10 +6,3 @@ component=user
 
 func_nodejs
 
-echo -e "\e[35m>>>>>>>>>>>>copy mongo.repo file <<<<<<<<<<<\e[0m"
-cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
-echo -e "\e[35m>>>>>>>>>>>> installing mongodb  <<<<<<<<<<<\e[0m"
-yum install mongodb-org-shell -y
-echo -e "\e[35m>>>>>>>>>>>> creating mongodb schema <<<<<<<<<<<\e[0m"
-mongo --host mongodb.tej07.online </app/schema/user.js
-systemctl restart user
