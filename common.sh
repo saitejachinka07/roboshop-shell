36b2ca2a10d436056823c4a4b89b11274c1d00b4
saitej <chinka.saiteja@gmail.com>
2023-05-02 16:13:22 +0530

    [0m

--- common.sh
+++ common.sh
@@ -14,11 +14,11 @@ echo -e "\e[36m>>>>>>>>>>>>>>>> $1 <<<<<<<<<<<\e[0m"
 schema_setup(){
 if [ "$schema_setup" == "mongo" ]; then
 
- print_head copy mongo.repo file <<<<<<<<<<<\e[0m" 
+ print_head copy mongo.repo file   
 cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
- print_head installing mongodb   <<<<<<<<<<<\e[0m"
+ print_head installing mongodb    
 yum install mongodb-org-shell -y
- print_head creating mongodb schema  <<<<<<<<<<<\e[0m"
+ print_head creating mongodb schema   
 mongo --host mongodb.tej07.online </app/schema/$component.js
 systemctl restart user
 fi
