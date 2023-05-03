11dbb49aecdbf0327591b2952c7e0fe9f17ab781
saitej <chinka.saiteja@gmail.com>
2023-05-03 13:44:28 +0530

    code dry

--- common.sh
+++ common.sh
@@ -3,66 +3,126 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 
 
-print_head(){
+func_print_head(){
 
-echo -e "\e[36m>>>>>>>>>>>>>>>> $1 <<<<<<<<<<<\e[0m"
+1echo -e "\e[35m>>>>>>>>>>>>>>>> $1 <<<<<<<<<<<\e[0m"
 
 
 }
 
 
-schema_setup(){
+func_schema_setup(){
 if [ "$schema_setup" == "mongo" ]; then
 
-
-
-
-  print_head "copy mongo.repo file"  
+  func_print_head "copy mongo.repo file"  
   
-cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+  cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 
-    print_head  "installing mongodb"  
+    func_print_head  "installing mongodb"  
   
-yum install mongodb-org-shell -y  
-  print_head "creating mongodb schema"   
+  yum install mongodb-org-shell -y  
+  func_print_head "creating mongodb schema"   
  
-mongo --host mongodb.tej07.online </app/schema/$component.js
-  print_head "restarting $component service"  
+  mongo --host mongodb.tej07.online </app/schema/$component.js
+  func_print_head "restarting $component service"  
 
-systemctl restart $component
+   systemctl restart $component
 fi
+
+if [ "$schema_setup" == 'mysql' ]; then
+
+  func_print_head "copying mysql repo file"
+  cp ${script_path}/mysql.repo  /etc/yum.repos.d/mysql.repo
+
+  func_print_head "Downloading mysql"
+  yum install mysql -y
+
+  func_print_head "Loading mysql schema"
+  mysql -h mysql.tej07.online -uroot -p${mysql_password} < /app/schema/$component.sql
+
+
+ fi
+
+
 
 }
 
+func_app_prereq(){
+
+func_print_head "Creat Application User"
+useradd ${app_user}
+
+func_print_head "Removing the /app folder"
+rm -rf /app
+
+func_print_head "Creat Application Directory"
+mkdir /app
+
+func_print_head "Downloading Application content"
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip 
+
+func_print_head "Redirecting Application Directory "
+cd /app 
+
+func_print_head "Extracting Application content"
+unzip /tmp/${component}.zip
+}
+
+
+
+
+func_systemd_setup(){
+func_print_head "copying ${component}.service file"
+cp ${script_path}/${component}.service  /etc/systemd/system/${component}.service
+
+func_print_head "Realoding the daemon"
+systemctl daemon-reload 
+
+func_print_head "Enabling the ${component} "
+systemctl enable ${component}"
+
+func_print_head "restarting ${component}"
+systemctl restart ${component}
+}
  
+
 
 
 func_nodejs() {
-  print_head   "creating nodejs repo file"
+  func_print_head   "creating nodejs repo file"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-  print_head "installing nodejs" 
+  func_print_head "installing nodejs" 
 yum install nodejs -y
-  print_head "creating roboshop user" 
-useradd ${app_user}
- print_head "removing /app folder" 
-rm -rf /app
- print_head "creating application folder" 
-mkdir /app
- print_head "downloading app content"
-curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip 
-cd /app 
- print_head "unziping the app content" 
-unzip /tmp/${component}.zip
- print_head "installing nodejs dependencies" 
+
+ func_app_prereq
+
+ func_print_head "installing nodejs dependencies" 
 npm install
- print_head "copying service file "
-cp ${script_path}/${component}.service  /etc/systemd/system/${component}.service
- print_head "realoading daemon" 
-systemctl daemon-reload
- print_head "enable user"  
-systemctl enable ${component}
-systemctl start ${component}
-print_head "schema_setup"
-schema_setup
+
+func_print_head "systemd_setup"
+func_systemd_setup
+
+func_print_head "schema_setup"
+func_schema_setup
+}
+
+
+
+func_java(){
+
+func_print_head "Installing maven"
+yum install maven -y
+
+func_app_prereq
+
+func_print_head "Downloading the java dependencies"
+cd /app 
+mvn clean package 
+mv target/${component}-1.0.jar ${component}.jar
+
+func_schema_setup
+
+func_systemd_setup
+
 }
